885561a4b29eeef664de03fef7126f4ad4ed7ab0
Arpit Shrivastava <152637028+Arpit-Shrivastva@users.noreply.github.com>
2024-12-20 10:37:02 +0530

    Update ScoreService.java

--- Cricker-webscrapping/src/main/java/com/example/cric/Cricker_webscrapping/Services/ScoreService.java
+++ Cricker-webscrapping/src/main/java/com/example/cric/Cricker_webscrapping/Services/ScoreService.java
@@ -5,8 +5,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.springframework.stereotype.Service;
-
-
 import java.io.IOException;
 
 @Service
